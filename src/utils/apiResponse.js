a11ff1b2e404cfe72f45bda7d739d6482d2a7cbf
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-07 11:16:53 +0530

    custom class for standardising the api responses generated

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,9 @@
+class apiResponse {
+  constructor(statuscode, message = "success", data) {
+    this.statuscode = statuscode
+    this.message = message
+    this.data = data
+    this.success=statuscode<400
+  }
+}
+export {apiResponse}
