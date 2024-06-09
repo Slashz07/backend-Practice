b6e9ec49db62e3afe4fddb7c6b10de7e40779e95
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-09 18:25:15 +0530

    multer middleware disk storage  property defined

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination:function (req, file, cb) {
+    cb(null,"../../public/temp")
+  },
+  filename: function (req, file, cb) {
+    const uniqueName=Date.now + '-' + Math.round(Math.random* 1E9)
+    cb(null,file.fieldname+uniqueName)//fieldname gives the name provided in the name attribute of the  input tag of type="file"
+  }
+})
+
+export const upload= multer({storage:storage})
