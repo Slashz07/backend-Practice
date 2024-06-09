6f5f37f45a7a0c77e13125d614c0ce12c5de453e
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-09 16:29:02 +0530

    method to upload file on cloudinary once multer provides the path of file saved temporarily on local server

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (uploadFileUrl) => {
+  try {
+    if (!uploadFileUrl) return null
+    
+    const response= await cloudinary.uploader.upload(uploadFileUrl, {
+      resource_type: 'auto'
+    })
+    console.log("File has been successfully uploaded on Cloudinary! Access it on: ", response.url)
+
+    return response
+
+  } catch {
+    fs.unlinkSync(uploadFileUrl)
+    return null
+  }
+
+}
+
+export {uploadOnCloudinary}
