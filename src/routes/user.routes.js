e92d052ec76cc22589174eff7da117000da0e8f9
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-14 22:58:40 +0530

    multer middleware provided for handling files submitted by user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router=Router()
 
-router.route("/register").post(registerUser) //the functions invoked in .post() or .get() etc recieve parameters req,res,next and err but function that accepts err as parameter is only invoked if error occurs
+router.route("/register").post(
+  upload.fields([//we use .single  when we only havw to upload a single file in the entire form
+    //and we use .array when multiple files are to be uploaded from a single field in the form
+  //.fields intakes an array of multiple objects,each object representing a form field that intakes as many files as the maxCount says.The name provided in the object must be same as thst in form field
+    {
+      name: "avatar",
+      maxCount:1
+    },
+    {
+      name: "coverImage",
+      maxCount:1
+    }
+  ]),
+  registerUser) //the functions invoked in .post() or .get() etc recieve parameters req,res,next and err but function that accepts err as parameter is only invoked if error occurs else its ignored ,so carefully choose what parameters will the function invoked will accept
 
 export default router
