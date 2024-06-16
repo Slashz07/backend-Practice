5d216511510a0a7ea88129d0f4d6658289cc0a45
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-16 11:48:46 +0530

    temporary file storage path corrected and minor syntax errors resolved

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,10 +2,10 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination:function (req, file, cb) {
-    cb(null,"../../public/temp")
+    cb(null,"./public/temp")
   },
   filename: function (req, file, cb) {
-    const uniqueName=Date.now + '-' + Math.round(Math.random* 1E9)
+    const uniqueName=Date.now() + '-' + Math.round(Math.random()* 1E9)
     cb(null,file.fieldname+uniqueName)//fieldname gives the name provided in the name attribute of the  input tag of type="file"
   }
 })
