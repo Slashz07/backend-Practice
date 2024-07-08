804c5c5c396ea18a5dab8a0288dcde7dd2f1995b
Slashz07 <sumitforsaken76248@gmail.com>
2024-07-08 23:07:11 +0530

    debug optimisations and user channel info aggregate pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { wrapper } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt, { decode } from "jsonwebtoken";
 import { v2 as cloudinary } from 'cloudinary';
+import { mongo } from "mongoose";
 
 
 const generateAccessAndRefreshToken = async (userId) => {
@@ -135,8 +136,8 @@ const logoutUser = wrapper(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined//refresh token removed from database
+      $unset: {
+        refreshToken: 1//here whatever value is flagged with one ,it is  removed from database by unset
       }
     },
     {
@@ -157,69 +158,74 @@ const logoutUser = wrapper(async (req, res) => {
 const refreshAccessToken = wrapper(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
   if (!incomingRefreshToken) {
-    throw new apiError(401,"Unauthorised request")
+    throw new apiError(401, "Unauthorised request")
   }
 
- try {//its good practice to wrap decoding code in try-catch since its error prone
-   const decodedRefreshToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
-   const user = await User.findById(decodedRefreshToken?._id)
-   if (!user) {
-     throw new apiError(401,"invalid refresh token")
-   }
-   if (incomingRefreshToken !== user.refreshToken) {
-     throw new apiError("Refresh token is expired or used")
-   }
- 
-   const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(user._id)//used newRefreshToken rather then refreshToken since here we are creating a new refresh token which should not be consfused with older value because of same name
-   const options = {
-     secure: true,
-     httpOnly:true
-   }
-   return res.status(200)
-     .cookies("accessToken", accessToken)
-     .cookies("refreshToken", newRefreshToken)
-     .json(
-       new apiResponse(
-         200,
-         {accessToken,refreshToken:newRefreshToken},
-         "Access tokens refreshed "
-     )
-   )
- } catch (error) {
-  throw new apiError(401,error?.message||"Invalid Refresh Token")
- }
+  try {//its good practice to wrap decoding code in try-catch since its error prone
+    const decodedRefreshToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+    const user = await User.findById(decodedRefreshToken?._id)
+    if (!user) {
+      throw new apiError(401, "invalid refresh token")
+    }
+   
+      console.log(incomingRefreshToken)
+      console.log(user.refreshToken)
+    
+    if (incomingRefreshToken !== user.refreshToken) {
+      throw new apiError("Refresh token is expired or used")
+    }
+
+    const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(user._id)//used newRefreshToken rather then refreshToken since here we are creating a new refresh token which should not be consfused with older value because of same name
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+    
+    return res.status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new apiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access tokens refreshed "
+        )
+      )
+  } catch (error) {
+    throw new apiError(401, error?.message || "Invalid Refresh Token")
+  }
 })
 
 const changePassword = wrapper(async (req, res) => {
   const { oldPassword, newPassword, confirmPassword } = req.body
   if (newPassword !== confirmPassword) {
-    throw new apiError(401,"password confirmation failed")
+    throw new apiError(401, "password confirmation failed")
   }
   const user = await User.findById(req.user._id)
   const isPasswordCorrect = await user.checkPassword(oldPassword, this.password)
   if (!isPasswordCorrect) {
-    throw new apiError(401,"Incorrect old password")
+    throw new apiError(401, "Incorrect old password")
   }
   user.password = newPassword
   await user.save({ validateBeforeSave: false })
   return res.status(200)
     .json(
-               new apiResponse(200,{},"Password changed successfully")
-             )
+      new apiResponse(200, {}, "Password changed successfully")
+    )
 })
 
 const getCurrentUser = wrapper(async (req, res) => {
   return res.status(200).json(
-    200,req?.user,"User data fetched successfully"
+    200, req?.user, "User data fetched successfully"
   )
 })
 
 const updateUserData = wrapper(async (req, res) => {
   const { fullName, email } = req.body
   if (!(fullName && email)) {
-    throw new apiError(400,"All fields are required")
+    throw new apiError(400, "All fields are required")
   }
-  const user=await User.findByIdAndUpdate(req.user._id,
+  const user = await User.findByIdAndUpdate(req.user._id,
     {
       $set: {
         userName,
@@ -227,48 +233,55 @@ const updateUserData = wrapper(async (req, res) => {
       }
     },
     {
-      new: true
+      new: true//ensures that updated user is returned and saved in the database collection
     }
   ).select("-password -refreshToken")
 
   return res.status(200).json(
-    200,user,"Account details updated successfully"
-  )  
+    200, user, "Account details updated successfully"
+  )
 })
 
 const updateAvatar = wrapper(async (req, res) => {
   const avatarUrl = req.file?.path//since multer here only intakes a single file..we use file instead of files
   if (!avatarUrl) {
-    throw new apiError(400,"Avatar file is missing")
+    throw new apiError(400, "Avatar file is missing")
   }
   const avatarClodinary = await uploadOnCloudinary(avatarUrl)
 
   if (!avatarClodinary.url) {
     throw new apiError(400, "Error while uploading avatar file on cloudinary")
   }
-  const user = await User.findById(req.user._id)
+  let user = await User.findById(req.user._id)
 
   let currentAvatarPublicId = user.avatar.substring(user.avatar.lastIndexOf("/") + 1)
-  currentAvatarPublicId = currentAvatarPublicId.substring(0,currentAvatarPublicId.lastIndexOf("."))
+  currentAvatarPublicId = currentAvatarPublicId.substring(0, currentAvatarPublicId.lastIndexOf("."))
 
-  const res=cloudinary.uploader.destroy(currentAvatarPublicId, {
-  resource_type:"auto"
+  const response = cloudinary.uploader.destroy(currentAvatarPublicId, {
+    resource_type: "auto"
   })
-  if (res.result == "ok") {
+  if (response.result === "ok") {//.destroy returns an object with result:"ok" or  result:"error..."
     console.log("old avatar image successfully deleted from cloudinary")
   }
-  user.avatar = avatarClodinary.url
-  user = user.save({ validateBeforeSave: false }).select("-password -refreshToken")
-  
+
+  user = await User.findByIdAndUpdate(user._id, {
+    $set: {
+      avatar: avatarClodinary.url
+    }
+  }, {
+    new: true
+  }).select("-password -refreshToken"); // Select fields after update
+
+
   return res.status(200).json(
-    200,user,"User avatar changed successfully"
+    200, user, "User avatar changed successfully"
   )
 })
 
 const updateCoverImage = wrapper(async (req, res) => {
   const coverImageUrl = req.file?.path//since multer here only intakes a single file..we use file instead of files
   if (!coverImageUrl) {
-    throw new apiError(400,"Cover Image file is missing")
+    throw new apiError(400, "Cover Image file is missing")
   }
   const coverImageClodinary = await uploadOnCloudinary(coverImageUrl)
 
@@ -280,36 +293,36 @@ const updateCoverImage = wrapper(async (req, res) => {
   let currentCoverImagePublicId = user.avatar.substring(user.avatar.lastIndexOf("/") + 1)
   currentCoverImagePublicId = currentCoverImagePublicId.substring(0, currentCoverImagePublicId.lastIndexOf("."))
 
-  const res = cloudinary.uploader.destroy(currentCoverImagePublicId, {
+  const response = cloudinary.uploader.destroy(currentCoverImagePublicId, {
     resource_type: "auto"
   })
-  if (res.result == "ok") {
+  if (response.result == "ok") {
     console.log("old  cover-image successfully deleted from cloudinary")
   }
   user.coverImage = coverImageClodinary.url
   user = user.save({ validateBeforeSave: false }).select("-password -refreshToken")
-  
+
   return res.status(200).json(
-    200,user,"User Cover-Image changed successfully"
+    200, user, "User Cover-Image changed successfully"
   )
 })
 
 const getUserChannelInfo = wrapper(async (req, res) => {
-  const userName = req.params
+  const { userName } = req.params
   if (!userName?.trim()) {
     throw new apiError(400, "username is missing")
   }
-  const channelInfo=await User.aggregate(
+  const channelInfo = await User.aggregate(
     [
       {
-        $match:{userName:userName}
+        $match: { userName: userName }
       },
       {
         $lookup: {
           from: "subscriptions",
           localField: "_id",
           foreignField: "channel",
-          as:"subscribers"
+          as: "subscribers"
         }
       },
       {
@@ -323,21 +336,90 @@ const getUserChannelInfo = wrapper(async (req, res) => {
       {
         $addFields: {
           subscriberCount: {
-            $size:"$subscribers"
+            $size: "$subscribers"
           },
           subscribedToCount: {
-            $size:"$subscribedTo"
+            $size: "$subscribedTo"
           },
           isSubscribed: {
             $cond: {
               if: { $in: [req.user?._id, "$subscribers.subscriber"] },
               then: true,
-              else:false
+              else: false
             }
           }
         }
+      },//by now we have an object containing the user model instance info for "username" provided in url,an array ofobjects provided by first lookup and an array of objectsproovided by second lookup
+
+      // in order to decide which value should be provided for channel profile we user-->
+      {
+        $project: {
+          fullName: 1,
+          userName: 1,
+          email: 1,
+          avatar: 1,
+          coverImage: 1,
+          subscriberCount: 1,
+          subscribedToCount: 1,
+          isSubscribed: 1,
+          createdAt: 1
+        }
       }
-    ]
+    ])
+  if (!channelInfo?.length) {
+    throw new apiError(401, "channel doesn't exist")
+  }
+
+  return res.status(200).json(
+    200, channelInfo[0], "channel information successfully retrieved"
+  )
+})
+
+const getWatchHistory = wrapper(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "Video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "user",
+              localStorage: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    userName: 1,
+                    fullName: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res.status(200).json(
+    new apiResponse(200, user.getWatchHistory, "User watch history fetched successfully ")
   )
 })
 
@@ -349,5 +431,8 @@ export {
   changePassword,
   getCurrentUser,
   updateAvatar,
-  updateCoverImage
+  updateUserData,
+  updateCoverImage,
+  getUserChannelInfo,
+  getWatchHistory
 }
